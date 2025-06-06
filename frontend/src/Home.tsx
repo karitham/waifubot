1d821c5a43f8232b8feb16b9af78aea15359dafc
karitham <kar@karitham.dev>
2025-06-06 23:41:39 +0000

    frontend: fix Home

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -1,48 +1,49 @@
 import { type Navigator, useNavigate } from "@solidjs/router";
 import { getUser } from "./api/list";
-import GhostButton from "./components/generic/GhostButton";
 import Icon from "/src/assets/icon.png";
-import { Input } from "./components/generic/Input";
 import { createSignal } from "solid-js";
+import { TextField } from "@kobalte/core/text-field";
+import { Button } from "@kobalte/core/button";
 
 const userOrList = async (nav: Navigator, id: string) => {
-	if (id.match(/\d{6,}/)) return nav(`/list/${id}`);
-	const { data: user, error } = await getUser(id);
-	if (error) {
-		console.error(error);
-		return;
-	}
+  if (id.match(/\d{6,}/)) return nav(`/list/${id}`);
+  const { data: user, error } = await getUser(id);
+  if (error) {
+    console.error(error);
+    return;
+  }
 
-	if (user.id) return nav(`/list/${user.id}`);
+  if (user.id) return nav(`/list/${user.id}`);
 };
 
 export default () => {
-	const nav = useNavigate();
+  const nav = useNavigate();
+  const [value, setValue] = createSignal("");
 
-	const [value, setValue] = createSignal("");
-
-	const Bar = () => (
-		<div class="flex flex-col gap-2">
-			<Input
-				label="Discord ID or Anilist user"
-				placeholder="Kar"
-				onInput={setValue}
-				onEnter={() => userOrList(nav, value())}
-			/>
-		</div>
-	);
-
-	return (
-		<main class="bg-base h-screen w-screen font-sans selection:bg-overlayC">
-			<div class="flex flex-col gap-16 pt-36 items-center justify-center text-text">
-				<img src={Icon} alt="icon" />
-				<div class="flex flex-row gap-4 items-end bg-mantle p-6 rounded-xl">
-					<Bar />
-					<GhostButton onClick={() => userOrList(nav, value())} type="submit">
-						Search
-					</GhostButton>
-				</div>
-			</div>
-		</main>
-	);
+  return (
+    <main class="bg-base h-screen w-screen font-sans selection:bg-overlayC">
+      <div class="flex flex-col gap-16 pt-32 items-center justify-center text-text">
+        <img src={Icon} alt="icon" />
+        <div class="text-sm flex flex-row gap-4 items-end bg-mantle p-4 rounded-xl">
+          <TextField onChange={setValue} class="w-64 flex flex-col gap-2">
+            <TextField.Label class="text-sm text-subtextA">
+              Discord ID or Anilist username
+            </TextField.Label>
+            <TextField.Input
+              class="w-full p-4 text-sm rounded-md focus:outline-none bg-surfaceA placeholder:font-sans border-none hover:cursor-text placeholder:text-overlayC text-text overflow-clip"
+              onKeyDown={(e) => e.key === "Enter" && userOrList(nav, value())}
+              placeholder="Kar"
+            />
+          </TextField>
+          <Button
+            class="rounded-md font-sans border-none hover:cursor-pointer bg-surfaceA text-text p-4 focus:outline-none"
+            onClick={() => userOrList(nav, value())}
+            type="button"
+          >
+            Go
+          </Button>
+        </div>
+      </div>
+    </main>
+  );
 };
